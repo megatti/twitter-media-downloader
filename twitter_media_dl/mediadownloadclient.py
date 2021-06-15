6108a64f6833b587784ba5b30ad9d6c061003d46
Gwion Nye <gwionnyeuk@gmail.com>
2021-06-15 14:14:24 +0100

    Split into class file and runner file.

--- twitter_media_dl/mediadownloadclient.py
+++ twitter_media_dl/mediadownloadclient.py
@@ -242,31 +242,3 @@ class ImageDownloadClient(peony.PeonyClient):
             await download_file(filename, media_details, self._session, 
                                 base_folder=os.path.join(self.base_folder, "likes"))
             self.img_urls.add(media_details["url"])
-
-
-if __name__ == "__main__":
-    try:
-        print("Beginning to download media...")
-        base_folder = os.path.join(os.path.dirname(__file__), "..", "media")
-        kwargs = {"consumer_key":CONSUMER_KEY, 
-                  "consumer_secret":CONSUMER_SECRET, 
-                  "access_token":ACCESS_TOKEN, 
-                  "access_token_secret":ACCESS_TOKEN_SECRET, 
-                  "base_folder":base_folder}
-        my_client = ImageDownloadClient(**kwargs)
-        my_client.run()
-    finally:
-        print("Saving data...")
-
-        # Move current history to backup folder
-        os.makedirs(os.path.join(base_folder, "img_urls_backups"), exist_ok=True)
-        current_img_urls = glob.glob(os.path.join(base_folder, "img_urls-*.txt"))
-        for img_url_file in current_img_urls:  # could be multiple files?? shouldn't be though
-            os.rename(img_url_file, os.path.join(base_folder, "img_urls_backups", img_url_file))
-        
-        # Save new history
-        current_time = datetime.datetime.now().strftime("%Y-%m-%d-%H%M%S")
-        new_filename = os.path.join(base_folder, f"img_urls-{current_time}.txt") # Make new version's filename
-        with open(new_filename, "w") as file:  # Save as a text file to read later
-            for url in my_client.img_urls:
-                file.write(url + "\n")
