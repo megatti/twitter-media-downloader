a06a6825678dffba4980a62b6ec8094176faa27a
megatti <73905456+megatti@users.noreply.github.com>
2021-07-08 18:18:49 +0100

    Remove access token environment variable

--- twitter_media_dl/twitter_media_dl.py
+++ twitter_media_dl/twitter_media_dl.py
@@ -13,8 +13,6 @@ try:
     TWITTER_ID = os.environ["TWITTER_ID"]
     CONSUMER_KEY = os.environ["CONSUMER_KEY"]
     CONSUMER_SECRET = os.environ["CONSUMER_SECRET"]
-    ACCESS_TOKEN = os.environ["ACCESS_TOKEN"]
-    ACCESS_TOKEN_SECRET = os.environ["ACCESS_TOKEN_SECRET"]
     BEARER_TOKEN = os.environ["BEARER_TOKEN"]
 except KeyError:
     raise Exception("Not all required environment variables have been defined.")
